ad96f3dcf9835aec9ff8b30b8159fce24d8d4bbc
việt hồ đức <ducvietsqt@gmail.com>
2019-01-02 00:51:03 +0700

    update source

--- app/js/start.js
+++ app/js/start.js
@@ -1,78 +1,19 @@
 
 $(document).ready(function () {
-    deliveryApp.barStyleClick();
-    deliveryApp.datePicker();
-
-
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            //options here
+            autoScrolling:true,
+            scrollHorizontally: true,
+            navigation: true,
+            scrollOverflow: true,
+            sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+        });
+        //methods
+        $.fn.fullpage.setAllowScrolling(true);
+    });
 });
 
-
-var deliveryApp = (function ($) {
-    var barStyleClick = function (text) {
-        $('.bar-option--icon').on('click', function(e) {
-            e.preventDefault();
-            if($(this).hasClass('active')) return false;
-            $(this).addClass('active');
-            $(this).siblings().removeClass('active');
-            if($('.control-week').hasClass('hasWeek')) {
-                $('.control-week, .container_row').removeClass('hasWeek');
-            }else {
-                $('.control-week, .container_row').addClass('hasWeek');
-            }
-            e.stopPropagation();
-        })
-
-    };
-    var datePicker = function() {
-        try{
-            var _RANGE = 7;
-            var _elm = $( "#datepicker" );
-            var _elmText = $( ".control-week--title" );
-            $(function(){
-                _elm.datepicker();
-                _elm.datepicker( "setDate", new Date());
-                var next_date =  moment(new Date()).add(_RANGE,'days').format('YYYY-MM-DD');
-                var _str = moment(new Date()).format('MMMM D') + ' - ' + moment(next_date).format('MMMM D, YYYY');
-                _elmText.html(_str);
-            });
-            $('.icon_date').on('click', function (e) {
-                e.preventDefault();
-                var _ctl = $(this).attr('data-date');
-                var _date = _elm.datepicker( "getDate");
-                var next_date =  moment(_date).add(_RANGE,'days').format('YYYY-MM-DD');
-                var prev_date =  moment(_date).add(-_RANGE,'days').format('YYYY-MM-DD');
-                switch (_ctl) {
-                    case 'next':
-                        _elm.datepicker("setDate", new Date(next_date));
-                        // November 4 - 10, 2018
-                        var _str = moment(_date).format('MMMM D') + ' - ' + moment(next_date).format('MMMM D, YYYY');
-                        _elmText.html(_str);
-                        break;
-                    case 'prev':
-                        _elm.datepicker("setDate", new Date(prev_date));
-                        var _str = moment(_date).format('MMMM D') + ' - ' + moment(prev_date).format('MMMM D, YYYY');
-                        _elmText.html(_str);
-                        break;
-                    default:
-                        break;
-                }
-                e.stopPropagation();
-                return false;
-            })
-
-        }catch (e) {
-            console.log('datetimepicker', e.message);
-        }
-    }
-    var onChangeDate = function() {};
-    window.deliveryApp = deliveryApp;
-    return {
-        barStyleClick: barStyleClick,
-        datePicker: datePicker,
-        onChangeDate: onChangeDate
-    };
-
-})($);
 
 
 var isMobile = {
